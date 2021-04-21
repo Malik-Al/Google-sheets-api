4ea5c94b16b4cd883fc47a7c1ed2695b45269ab6
imenov-a1@mail.ru <malikimenov@gmail.com>
2021-04-21 17:20:26 +0600

    google sheets api 'get list'

---
+++ spreadsheets.js
@@ -0,0 +1,22 @@
+const {google} = require ('googleapis');
+const keys = require ('./keys.json');
+
+const client = new google.auth.JWT (keys.client_email, null, keys.private_key, ['https://www.googleapis.com/auth/spreadsheets']);
+
+client.authorize(function(error,tokens){
+  if(error){console.log(error);return;}
+  else
+  {console.log('connected…'),gsrun (client)}
+});
+
+async function gsrun(cl){ //cl for client
+  const gsapi=google.sheets({version:'v4', auth:cl });
+  const opt={
+    spreadsheetId: '1cH0VLuRxn886VMXB3-aNUe6nBvnViDU66nyxN8xGJIo',
+    range:'Лист1!A1:E5'
+};
+  let dataObtained= await gsapi.spreadsheets.values.get(opt);
+  console.log(dataObtained.data.values);
+}
+
+
