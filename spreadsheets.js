748136ac45298441468fd6e4e537ac7b05becda7
imenov-a1@mail.ru <malikimenov@gmail.com>
2021-04-26 20:23:03 +0600

    add for data sheets name

--- spreadsheets.js
+++ spreadsheets.js
@@ -2,35 +2,39 @@ const {google} = require ('googleapis');
 const keys = require ('./keys.json');
 
 const client = new google.auth.JWT(keys.client_email, null, keys.private_key, [ 'https://www.googleapis.com/auth/spreadsheets' ]);
-client.authorize(function (error, tokens) {
+client.authorize(async function (error, tokens) {
   if (error) {
     console.log(error);
   }
-    console.log('connected…'),
-    gsrun(client);
-
-
 });
+  // for data
+  const array = {};
+  gsrunDB(client)
+    .then(async (list) => {
+      for (const item of list) {
+        const result = await gsrun(client,item);
+        array[item] = result;
+      }
+      console.log(array);
+    })
 
   // Sheets data
-  async function gsrun(cl) { //cl for client
+  async function gsrun(cl, name) { //cl for client
     const gsapi = google.sheets({ version: 'v4', auth: cl });
     const opt = {
       spreadsheetId: '1cH0VLuRxn886VMXB3-aNUe6nBvnViDU66nyxN8xGJIo',
-      range:'Лист1'
-
+      range: name,
     };
-    let dataObtained = await gsapi.spreadsheets.values.get(opt);
-    let dataOne = dataObtained.data.values[0]
-    let dataArray = dataObtained.data.values
-    await dataArray.shift()
-    const objects = dataArray.map(
+    const dataObtained = await gsapi.spreadsheets.values.get(opt);
+    const dataOne = dataObtained.data?.values?.[0]
+    const dataArray = dataObtained.data.values
+    await dataArray?.shift()
+    const objects = dataArray?.map(
       student => Object.fromEntries(
         student.map((value, index) => [dataOne[index], value])
       )
     )
-    console.log(objects)
-
+    return objects
   }
 
   // Name sheets
@@ -39,9 +43,9 @@ client.authorize(function (error, tokens) {
     const opt = {
       spreadsheetId: '1cH0VLuRxn886VMXB3-aNUe6nBvnViDU66nyxN8xGJIo',
     };
-    let dataOb = (await gsapi.spreadsheets.get(opt)).data.sheets;
-    let res = dataOb.map(({ properties: { title } }) => title)
-    console.log(res);
+    const dataOb = (await gsapi.spreadsheets.get(opt)).data.sheets;
+    const res = dataOb.map(({ properties: { title } }) => title)
+    return res
   }
 
 
