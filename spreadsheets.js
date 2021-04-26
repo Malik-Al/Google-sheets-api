8be33a9c9ee0578d4c57fab803b89d2ca294016c
imenov-a1@mail.ru <malikimenov@gmail.com>
2021-04-26 18:54:33 +0600

    add sheets data, sheets name

--- spreadsheets.js
+++ spreadsheets.js
@@ -5,32 +5,49 @@ const client = new google.auth.JWT(keys.client_email, null, keys.private_key, [ 
 client.authorize(function (error, tokens) {
   if (error) {
     console.log(error);
-    return;
-  } else {
-    console.log('connected…'),
-    gsrun(client)
   }
+    console.log('connected…'),
+    gsrun(client);
+
+
 });
 
+  // Sheets data
+  async function gsrun(cl) { //cl for client
+    const gsapi = google.sheets({ version: 'v4', auth: cl });
+    const opt = {
+      spreadsheetId: '1cH0VLuRxn886VMXB3-aNUe6nBvnViDU66nyxN8xGJIo',
+      range:'Лист1'
 
-async function gsrun(cl) { //cl for client
-  const gsapi = google.sheets({ version: 'v4', auth: cl });
-  const opt = {
-    spreadsheetId: '1cH0VLuRxn886VMXB3-aNUe6nBvnViDU66nyxN8xGJIo',
-    range: 'Лист1!A1:E5'
-  };
-  let dataObtained = await gsapi.spreadsheets.values.get(opt);
-  let dataOne = dataObtained.data.values[0]
-  let dataArray = dataObtained.data.values
-  dataArray.shift()
-  const objects = dataArray.map(
-    student => Object.fromEntries(
-      student.map((value, index) => [dataOne[index], value])
+    };
+    let dataObtained = await gsapi.spreadsheets.values.get(opt);
+    let dataOne = dataObtained.data.values[0]
+    let dataArray = dataObtained.data.values
+    await dataArray.shift()
+    const objects = dataArray.map(
+      student => Object.fromEntries(
+        student.map((value, index) => [dataOne[index], value])
+      )
     )
-  )
-  console.log(objects);
+    console.log(objects)
 
-}
+  }
+
+  // Name sheets
+  async function gsrunDB(cl) { //cl for client
+    const gsapi = google.sheets({ version: 'v4', auth: cl });
+    const opt = {
+      spreadsheetId: '1cH0VLuRxn886VMXB3-aNUe6nBvnViDU66nyxN8xGJIo',
+    };
+    let dataOb = (await gsapi.spreadsheets.get(opt)).data.sheets;
+    let res = dataOb.map(({ properties: { title } }) => title)
+    console.log(res);
+  }
+
+
+
+
+
 
 
 
